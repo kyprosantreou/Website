017a58015cd063e74f03b8eea042bffb122db0d8
Kypros Andreou <kyprosantreou@outlook.com>
2024-04-19 11:29:31 +0300

    new file:   Assets/contact.png 	new file:   Assets/home.png 	new file:   Assets/info.png 	new file:   Styles/SideBar.css 	modified:   Styles/welcome_page_style.css 	renamed:    Login.html -> Templates/Login.html 	renamed:    Register.html -> Templates/Register.html 	renamed:    index.html -> Templates/index.html 	new file:   Templates/test.html 	new file:   main.py 	new file:   static/Assets/contact.png 	new file:   static/Assets/eye-off.svg 	new file:   static/Assets/eye.svg 	new file:   static/Assets/home.png 	new file:   static/Assets/info.png 	new file:   static/Assets/moon.svg 	new file:   static/Assets/sun.svg 	new file:   static/Css/SideBar.css 	new file:   static/Css/login_style.css 	new file:   static/Css/register_style.css 	new file:   static/Css/welcome_page_style.css 	new file:   static/Js/password_toggle.js

---
+++ static/Js/password_toggle.js
@@ -0,0 +1,25 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var passwordToggle = document.querySelector(".toggle-password");
+    
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordToggle.style.backgroundImage = "url('../Assets/eye-off.svg')";
+    } else {
+        passwordInput.type = "password";
+        passwordToggle.style.backgroundImage = "url('../Assets/eye.svg')";
+    }
+}
+
+function toggleRepeatPasswordVisibility() {
+    var repeatPasswordInput = document.getElementById("repeat_password");
+    var repeatPasswordToggle = document.querySelector(".toggle-repeat-password");
+    
+    if (repeatPasswordInput.type === "password") {
+        repeatPasswordInput.type = "text";
+        repeatPasswordToggle.style.backgroundImage = "url('../Assets/eye-off.svg')";
+    } else {
+        repeatPasswordInput.type = "password";
+        repeatPasswordToggle.style.backgroundImage = "url('../Assets/eye.svg')";
+    }
+}
