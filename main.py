41878948057a77f60e0a777312d5fcc55b476f95
Kypros Andreou <kyprosantreou@outlook.com>
2024-04-25 20:09:40 +0300

    modified:   main.py 	modified:   static/Css/welcome_page_style.css

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, make_response
 from flask_mysqldb import MySQL
+import argon2
 
 app = Flask(__name__)
 
@@ -9,10 +10,12 @@ app.config['MYSQL_PASSWORD'] = 'Ionio2002@!'
 app.config['MYSQL_DB'] = 'task_flow'
 
 mysql = MySQL(app)
+ph = argon2.PasswordHasher()
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    theme_mode = request.cookies.get('theme_mode', 'light')
+    return render_template('index.html', theme_mode=theme_mode)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,37 +24,46 @@ def login():
         password = request.form['password']
         cur = mysql.connection.cursor()
 
-        cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
         user = cur.fetchone()
 
-        cur.close()
-
         if user:
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html', error='Invalid email or password.')
+            stored_password = user[4]  
+            try:
+                ph.verify(stored_password, password)
+                
+                response = make_response(redirect(url_for('index')))
+                
+                response.set_cookie('theme_mode', request.form.get('theme_mode', 'light'))
+                return response
+            except argon2.exceptions.VerifyMismatchError:
+                return render_template('login.html', error='Invalid email or password.')
+
+        cur.close()
 
     return render_template('login.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-   if request.method == 'POST':
-      name = request.form['Name']
-      surname = request.form['Surname']
-      username = request.form['Username']
-      email = request.form['Email']
-      password = request.form['password']
-      cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        name = request.form['Name']
+        surname = request.form['Surname']
+        username = request.form['Username']
+        email = request.form['Email']
+        password = request.form['password']
+        hashed_password = ph.hash(password)
+        cur = mysql.connection.cursor()
 
-      cur.execute(f"INSERT INTO users (name, surname, username, email, password) VALUES ('{name}', '{surname}', '{username}', '{email}', '{password}')")
-      
-      mysql.connection.commit()
+        cur.execute("INSERT INTO users (name, surname, username, email, password) VALUES (%s, %s, %s, %s, %s)",
+                    (name, surname, username, email, hashed_password))
+        
+        mysql.connection.commit()
+        cur.close()
 
-      cur.close()
-
-      return redirect(url_for('login'))
+        return redirect(url_for('login'))
    
-   return render_template('register.html')
+    return render_template('register.html')
+
 
 @app.route('/static/Templates/Profile.html')
 def profile():
@@ -59,4 +71,4 @@ def profile():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run() 
+    app.run()
